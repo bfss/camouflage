b8d13b7ba1d8910f28af573df4aea4edaf781bb7
bfss <bfss@bfss.com>
2023-05-09 21:19:50 +0800

    create appbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
@@ -6,10 +7,12 @@ import PrivateRoutes from "./components/PrivateRoutes";
 import Article from "./components/Article";
 import EditArticle from "./components/EditArticle";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import theme from "./components/theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <ResponsiveAppBar />
       <BrowserRouter>
         <Routes>
           <Route element={<Login />} path="/login"></Route>
@@ -21,7 +24,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        // mode: 'dark',
+        primary: {
+            light: '#ef5350',
+            main: '##F7F9F9',
+        },
+    },
+});
+
+export default theme;
